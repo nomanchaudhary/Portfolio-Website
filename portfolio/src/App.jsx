d51d9379469d95bfe5253769844b7166808021d2
Muhammad Noman <nomanchaudhary1618@gmail.com>
2024-11-18 00:08:36 +0500

    added vercel.json file

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Pages/Home";
